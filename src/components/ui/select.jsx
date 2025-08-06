a2c6c18ba29f317b5dd857bcdd92ca23c6688190
EstellePause <boiteapub5575@gmail.com>
2025-08-06 18:11:33 +0200

    Add files via upload

---
+++ src/components/ui/select.jsx
@@ -0,0 +1,2 @@
+export const Select = ({ value, onValueChange, children }) => <select value={value} onChange={(e) => onValueChange(e.target.value)}>{children}</select>;
+export const SelectItem = ({ value, children }) => <option value={value}>{children}</option>;
